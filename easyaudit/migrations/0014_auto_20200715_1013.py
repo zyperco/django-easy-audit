924c8e31390e534a85a3e300d556a57fa9df26f7
Richard Hanson <rhngit@users.noreply.github.com>
2020-07-15 10:17:24 -0700

    Change object_id to CharField

---
+++ easyaudit/migrations/0014_auto_20200715_1013.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("easyaudit", "0013_auto_20190723_0126"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="crudevent",
+            name="object_id",
+            field=models.CharFiled(max_length=255),
+        ),
+    ]
